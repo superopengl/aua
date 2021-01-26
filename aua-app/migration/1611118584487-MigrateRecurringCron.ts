a09c022b5a736b3438bbd1d6d30ef84dd3b48d95
Jun Shao <mr.shaojun@gmail.com>
2021-01-26 19:11:55 +1100

    tidy up

--- aua-app/migration/1611118584487-MigrateRecurringCron.ts
+++ aua-app/migration/1611118584487-MigrateRecurringCron.ts
@@ -2,8 +2,8 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class MigrateRecurringCron1611118584487 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+        public async up(queryRunner: QueryRunner): Promise<void> {
+                await queryRunner.query(`
 UPDATE public."recurring" 
 SET "startFrom" = '2021-06-30 05:00:00',
 "nextRunAt" = '2021-06-30 05:00:00',
@@ -12,7 +12,7 @@ period = 'year'
 WHERE cron = '0 0 0 30 6 *'
         `);
 
-        await queryRunner.query(`
+                await queryRunner.query(`
 UPDATE public."recurring" 
 SET "startFrom" = '2021-07-01 05:00:00',
 "nextRunAt" = '2021-07-01 05:00:00',
@@ -21,7 +21,7 @@ period = 'year'
 WHERE cron = '0 0 0 1 7 *'
         `);
 
-        await queryRunner.query(`
+                await queryRunner.query(`
 UPDATE public."recurring" 
 SET "startFrom" = '2021-03-31 05:00:00',
 "nextRunAt" = '2021-03-31 05:00:00',
@@ -30,17 +30,17 @@ period = 'month'
 WHERE cron = '0 0 0 L */3 *'
         `);
 
-        await queryRunner.query(`
+                await queryRunner.query(`
 UPDATE public."recurring" 
 SET "startFrom" = '2021-01-31 05:00:00',
 "nextRunAt" = '2021-01-31 05:00:00',
 "every" = 1,
 period = 'month'
 WHERE cron = '0 0 0 L */1 *'
-        `);        
-    }
+        `);
+        }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+        public async down(queryRunner: QueryRunner): Promise<void> {
+        }
 
 }
