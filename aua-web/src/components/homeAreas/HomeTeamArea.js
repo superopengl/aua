5072033605f2b6b442d3419655b96b89ad89573f
Jun Shao <mr.shaojun@gmail.com>
2021-03-05 18:56:59 +1100

    update team

--- aua-web/src/components/homeAreas/HomeTeamArea.js
+++ aua-web/src/components/homeAreas/HomeTeamArea.js
@@ -48,33 +48,15 @@ class HomeTeamArea extends React.Component {
     return (
       <HomeRowArea {...props} title="Team">
         <InfoCard >
-          <StyledAvatar src="/images/avatar-zhou.jpg" />
+          <StyledAvatar src="/images/avatar-chaire.jpg" />
           <section>
-            <Title level={3}>Jinlin Zhou</Title>
             <p>
-              Hi, I am the principal and partner in our accounting firm. I have more than 15 years accounting and taxation experience. Does it sound right for you?
-            </p>
+              Hi, I am Claire. I have been in Australia for more than 5 years， been working in our firm for more
+              than 3 years. With my life experience of crossing Australia in the past where I worked, I loved
+              and I enjoyed.</p>
             <p>
-              I like reading, sports, swimming, most of time if I am not working, you probably can find me nowhere but at home, watching soap opera.
-            </p>
-            <p>
-              I have a lovely family and living in northshore, hope somewhere, sometime, we just walk by on the street or catching up for a coffee.
-            </p>
-          </section>
-        </InfoCard>
-
-        <InfoCard >
-          <StyledAvatar src="https://static.wixstatic.com/media/8a7fac_45052d578d8348f0bb66ee06c96ac5c4~mv2.jpg/v1/fill/w_294,h_374,al_c,q_80,usm_0.66_1.00_0.01/0970526135807ea3126b40600733e9bc.webp" />
-          <section>
-            <Title level={3}>Claire Cheng</Title>
-            <p>
-              Hi, I am Claire. I have been in Australia for more than 5 years. I have been working in our firm for more than 3 years.
-            </p>
-            <p>
-              I have been crossing australia in the past where I worked, I loved and I enjoyed.
-            </p>
-            <p>
-              I am usually busy with numbers and paperworks. Thank you if you can call me or text me or even come to see me!
+              I am usually busy with numbers and paper works. Thank you if you can call me or text me or
+              even come to see me!
             </p>
           </section>
         </InfoCard>
@@ -82,23 +64,32 @@ class HomeTeamArea extends React.Component {
         <InfoCard >
           <StyledAvatar src="/images/avatar-pei.jpg" />
           <section>
-            <Title level={3}>Pei Zhang</Title>
             <p>
-              Hi, my name is Pei. I have been in Sydney for nearly 6 years, but I am originally from China. I studied
-              at the University of Sydney majoring in accounting and commercial law.
+              Hi, I am is Pei. I graduated from the University of Sydney and I am a self-motivated CPA
+              candidate as well I have been working as an accountant since I graduated and I am happy to join
+              our team. I am responsible for the bookkeeping, corporate accounting, and taxation matters.
             </p>
-            <p>I have been working as an
-            accountant since I graduated and I have been in our team for almost one year. I am responsible for
-            the bookkeeping, corporate accounting, and taxation matters.
-            </p>
-            <p>
-              I am into the work-life balance style
-              of our firm. When not in the office, you can find me spending time with my family.
+            <p>I am fond of the work-life balance style of our firm. When not in the office, you can find me
+            spending time with my family or playing the flute.
             </p>
           </section>
         </InfoCard>
 
-      </HomeRowArea>
+        <InfoCard >
+          <StyledAvatar src="/images/avatar-orange.jpg" />
+          <section>
+            <p>
+              Hi, I’m Orange. I originally from Malaysia. I have 4 years work experience in audit firm. I love to
+              explore culture and try different kind of food. My passion is singing. Feel free to join me if you
+              love singing too.
+            </p>
+            <p>
+              I’m a blogger, I love to record every moments of my life in my blog.
+            </p>
+          </section>
+        </InfoCard>
+
+      </HomeRowArea >
     );
   }
 }
